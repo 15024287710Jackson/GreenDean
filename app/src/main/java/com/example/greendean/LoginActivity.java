268ee750e24fa8a8f46e63966a693255b158861f
15024287710Jackson <42542657+15024287710Jackson@users.noreply.github.com>
2021-04-18 17:15:15 +0800

    登录记忆

--- app/src/main/java/com/example/greendean/LoginActivity.java
+++ app/src/main/java/com/example/greendean/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.greendean;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
 import android.os.Message;
@@ -24,52 +25,58 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
-    Button reg_button,login_button;
-    EditText user_name,user_pwd;
+    Button reg_button, login_button;
+    EditText user_name, user_pwd;
     private final int SUCCESS = 1;
     private final int FAILURE = 0;
     private final int ERRORCODE = 2;
-    int login_result=0;
+    int login_result = 0;
     String user_id;
+    String account = "";
+    String password = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user_name =(EditText)findViewById(R.id.et_account);
-        user_pwd =(EditText)findViewById(R.id.et_pwd);
-        login_button = (Button)findViewById(R.id.btn_log_login);
-        reg_button = (Button)findViewById(R.id.btn_log_reg);
+        user_name = (EditText) findViewById(R.id.et_account);
+        user_pwd = (EditText) findViewById(R.id.et_pwd);
+        login_button = (Button) findViewById(R.id.btn_log_login);
+        reg_button = (Button) findViewById(R.id.btn_log_reg);
+        loadPreferences();
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = user_name.getText().toString();
-                String pwd = user_pwd.getText().toString();
-                if(name.equals("")){
+                String name;
+                String pwd;
+                name = user_name.getText().toString();
+                pwd = user_pwd.getText().toString();
+                if (name.equals("")) {
                     Toast.makeText(LoginActivity.this, R.string.input_name, Toast.LENGTH_LONG).show();
-                }else if(pwd.equals("")){
+                } else if (pwd.equals("")) {
                     Toast.makeText(LoginActivity.this, R.string.input_pwd, Toast.LENGTH_LONG).show();
-                }
-                else{//sql查询
+                } else {//sql查询
                     System.out.println(2);
-                    new Thread(){
-                        public void run(){
-                            JSONObject jsonObject=loginSql(name,pwd);
+                    new Thread() {
+                        public void run() {
+                            JSONObject jsonObject = loginSql(name, pwd);
                             Looper.prepare();
                             try {
-                                if(jsonObject.getString("result").equals("Y")){
-                                    Toast.makeText(LoginActivity.this,jsonObject.getString("msg"), Toast.LENGTH_LONG).show();
-                                    login_result=1;
-                                    user_id=jsonObject.getString("id");
+                                if (jsonObject.getString("result").equals("Y")) {
+                                    Toast.makeText(LoginActivity.this, jsonObject.getString("msg"), Toast.LENGTH_LONG).show();
+                                    login_result = 1;
+                                    user_id = jsonObject.getString("id");
                                     String userName = name;
                                     String userId = user_id;
-                                    User user = new User(userId,userName);
-                                    Intent intent = ChatActivity.newIntent(LoginActivity.this,user.toString());
+                                    savePreferences(name, pwd);
+                                    User user = new User(userId, userName);
+                                    Intent intent = ChatActivity.newIntent(LoginActivity.this, user.toString());
                                     startActivity(intent);
                                     LoginActivity.this.finish();
-                                }else{
-                                    Toast.makeText(LoginActivity.this,jsonObject.getString("msg"), Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, jsonObject.getString("msg"), Toast.LENGTH_LONG).show();
                                 }
-                            }catch (Exception e){
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
                             Looper.loop();
@@ -108,9 +115,9 @@ public class LoginActivity extends AppCompatActivity {
         reg_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                    startActivity(intent);
-                    LoginActivity.this.finish();
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
             }
         });
     }
@@ -139,8 +146,11 @@ public class LoginActivity extends AppCompatActivity {
                 default:
                     break;
             }
-        };
+        }
+
+        ;
     };
+
     protected void JSONAnalysis(String string) {
         JSONObject object = null;
         try {
@@ -153,13 +163,14 @@ public class LoginActivity extends AppCompatActivity {
          */
         System.out.println(object);
     }
-    protected JSONObject loginSql(String edit_name,String edit_pwd)  {
+
+    protected JSONObject loginSql(String edit_name, String edit_pwd) {
         JSONObject jsonObject = null;
         int code;
-        try{
+        try {
             String path = "http://101.37.75.202:8081/resgisterLogin/loginUser";
             URL url = new URL(path);
-            HttpURLConnection conn =(HttpURLConnection) url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json; utf-8");
             conn.setConnectTimeout(5000);
@@ -180,13 +191,31 @@ public class LoginActivity extends AppCompatActivity {
                 response.append(responseLine.trim());
             }
             System.out.println(response.toString());
-            if(conn.getResponseCode()==200){
+            if (conn.getResponseCode() == 200) {
                 String str = response.toString();
-                jsonObject= new JSONObject(str);
+                jsonObject = new JSONObject(str);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return jsonObject;
+    }
+
+    public void savePreferences(String account, String password) {
+        SharedPreferences pref = getSharedPreferences("GreenDeanAccountInfo", MODE_PRIVATE);
+        pref.edit().putString("account", account).apply();
+        pref.edit().putString("password", password).apply();
+    }
+
+    public void loadPreferences() {
+        SharedPreferences pref = getSharedPreferences("GreenDeanAccountInfo", MODE_PRIVATE);
+        account = pref.getString("account", "");
+        password = pref.getString("password", "");
+        if (!account.equals("") && account != null) {
+            user_name.setText(account);
+        }
+        if (!password.equals("") && password != null) {
+            user_pwd.setText(password);
+        }
     }
 }
